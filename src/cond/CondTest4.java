39f96ecad3fd367259511cfc6b86e803c70588e3
Seongmin Park <snc01004@naver.com>
2024-02-03 10:37:54 +0900

    CondTest_4

---
+++ src/cond/CondTest4.java
@@ -0,0 +1,9 @@
+package cond;
+
+public class CondTest4 {
+    public static void main(String[] args) {
+        int x = 4;
+        String answer = (x%2==0)?"짝수":"홀수";
+        System.out.println(x+"는 "+answer);
+    }
+}
