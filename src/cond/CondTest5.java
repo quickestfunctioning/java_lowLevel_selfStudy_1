7d57d716beef5770b95a556aea28953cde840265
Seongmin Park <snc01004@naver.com>
2024-02-03 09:46:22 +0900

    Initial commit

---
+++ src/cond/CondTest5.java
@@ -0,0 +1,14 @@
+package cond;
+
+public class CondTest5 {
+    public static void main(String[] args) {
+        String grade = "A";
+
+        String speak = switch (grade){
+            case "A" -> "굿굿굿";
+            case "B" -> "흠";
+            default -> "..";
+        };
+        System.out.println(speak);
+    }
+}
