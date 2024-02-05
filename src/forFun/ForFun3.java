9de01024950d29cebbc73c59af93874bd324c508
Seongmin Park <snc01004@naver.com>
2024-02-05 22:31:56 +0900

    java / low-rank / self-study / 1

---
+++ src/forFun/ForFun3.java
@@ -0,0 +1,15 @@
+package forFun;
+
+import java.util.Scanner;
+
+public class ForFun3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = {90, 80, 70, 60, 50};
+
+        for (int arg : array) {
+            System.out.println(arg);
+        }
+    }
+}
